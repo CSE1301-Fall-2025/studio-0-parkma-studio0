393864a1207b0353daf56da11f89fe1b76a58ede
Nikki <m.nikki@wustl.edu>
2025-08-27 14:15:56 -0500

    hi

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,6 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println(" theasdl;fk   l;k j workshop! ");
 		System.out.println("Hello! VS Code finally works!!");
+		System.out.println("Hi! We finished on time!");
 	}   
 }
